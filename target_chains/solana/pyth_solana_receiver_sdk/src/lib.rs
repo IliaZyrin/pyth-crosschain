913ce2196a104635582d745c8fbd9a69e240070c
Ilia Zyrin <ilia.zyrin.e@gmail.com>
2026-06-01 14:32:58 +0200

    Trim receiver SDK to marginfi surface

--- target_chains/solana/pyth_solana_receiver_sdk/src/lib.rs
+++ target_chains/solana/pyth_solana_receiver_sdk/src/lib.rs
@@ -1,18 +1,10 @@
 // We can't do much about the size of `anchor_lang::error::Error`.
 #![allow(clippy::result_large_err)]
 
-use {
-    anchor_lang::{declare_id, prelude::Pubkey, pubkey},
-    borsh::{BorshDeserialize, BorshSerialize},
-    pythnet_sdk::wire::v1::MerklePriceUpdate,
-};
+use anchor_lang::{declare_id, prelude::Pubkey, pubkey};
 
-pub mod config;
-pub mod cpi;
 pub mod error;
-pub mod pda;
 pub mod price_update;
-pub mod program;
 
 cfg_if::cfg_if! {
     if #[cfg(feature = "lazer")] {
@@ -22,24 +14,4 @@ cfg_if::cfg_if! {
         declare_id!("rec5EKMGg6MxZYaMdyBfgwp4d5rB9T1VQH5pJv5LtFJ");
         pub const PYTH_PUSH_ORACLE_ID: Pubkey = pubkey!("pythWSnswVUd12oZpeFP8e9CVaEqJg25g1Vtc2biRsT");
     }
-}
-
-#[derive(Debug, BorshSerialize, BorshDeserialize, Clone)]
-pub struct PostUpdateParams {
-    pub merkle_price_update: MerklePriceUpdate,
-    pub treasury_id: u8,
-}
-
-#[derive(Debug, BorshSerialize, BorshDeserialize, Clone)]
-pub struct PostUpdateAtomicParams {
-    pub vaa: Vec<u8>,
-    pub merkle_price_update: MerklePriceUpdate,
-    pub treasury_id: u8,
-}
-
-#[derive(Debug, BorshSerialize, BorshDeserialize, Clone)]
-pub struct PostTwapUpdateParams {
-    pub start_merkle_price_update: MerklePriceUpdate,
-    pub end_merkle_price_update: MerklePriceUpdate,
-    pub treasury_id: u8,
 }
